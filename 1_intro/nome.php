6ceace47e807d896f78af0e14f270c2d2b4ddf25
FelipeSDS23 <felipe03san@gmail.com>
2023-10-06 10:43:01 -0300

    Enviando aulas 1 e 2

---
+++ 1_intro/nome.php
@@ -0,0 +1,5 @@
+<?php
+
+    echo "Felipe Silva";
+
+?>
---
+++ 2_conceitos_basicos/5_ex_2/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+$pessoa = "Felipe";
+
+echo $pessoa; // Pessoa 1
+echo "<br>";
+echo "programador <br>"; // Profissão
+echo "gamer <br>"; // Hobbie 
+
+?>
